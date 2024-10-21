3669bda37c37d185867277b83c5f2186f92742a6
nolate26 <nicolas.olate@uc.cl>
2024-10-21 10:48:04 -0300

    version final

--- pranna-project/src/TeamProgress.jsx
+++ pranna-project/src/TeamProgress.jsx
@@ -6,27 +6,40 @@ const TeamProgress = () => {
     const [team1Progress, setTeam1Progress] = useState(0);
     const [team2Progress, setTeam2Progress] = useState(0);
     const [team3Progress, setTeam3Progress] = useState(0);
+    const [winningTeam, setWinningTeam] = useState(null);
 
+    const handleWin = (teamName) => {
+        setWinningTeam(teamName);
+    };
 
     return (
         <div>
+            {winningTeam && (
+                <div className="winner">
+                    <h1>{`¡ ${winningTeam} ganador!`}</h1>
+                    <img src="copa.png" alt="Trophy" className="trophy" />
+                </div>
+            )}
             <ProgressBar
                 teamName="Equipo 1"
                 color="red"
                 progress={team1Progress}
                 onButtonClick={() => setTeam1Progress(team1Progress + 1)}
+                onWin={handleWin}
             />
             <ProgressBar
                 teamName="Equipo 2"
                 color="blue"
                 progress={team2Progress}
                 onButtonClick={() => setTeam2Progress(team2Progress + 1)}
+                onWin={handleWin}
             />
             <ProgressBar
                 teamName="Equipo 3"
                 color="green"
                 progress={team3Progress}
                 onButtonClick={() => setTeam3Progress(team3Progress + 1)}
+                onWin={handleWin}
             />
         </div>
     );
--- pranna-project/src/progressBar.jsx
+++ pranna-project/src/progressBar.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import './PaginaPrincipal.css';
 
-export const ProgressBar = ({ teamName, color, progress, onButtonClick }) => {
+export const ProgressBar = ({ teamName, color, progress, onButtonClick, onWin }) => {
     const handleButtonClick = () => {
-        if (progress < 19) {
-            onButtonClick();
-        } else {
-            onButtonClick();
-            alert(`${teamName} has won!`);
+        // si llega a 20 se despliega el mensaje de ganador
+        onButtonClick();
+        if (progress + 1 === 20) {
+            onWin(teamName);
         }
+
     };
 
     return (
         <div className="container">
             <div className="team-name">{teamName}</div>
-            <div className="progress-bar">
+            <div className="progress-bar" style={{ borderColor: color }}>
                 <div
                     className="progress-bar-fill"
                     style={{ width: `${(progress / 20) * 100}%`, backgroundColor: color }}
@@ -22,7 +22,7 @@ export const ProgressBar = ({ teamName, color, progress, onButtonClick }) => {
                     <span className="progress-label">{progress}</span>
                 </div>
             </div>
-            <button  onClick={handleButtonClick} className="button-85">+1 Punto!</button>
+            <button onClick={handleButtonClick} className="button-85">¡+1 Punto!</button>
         </div>
     );
 };
